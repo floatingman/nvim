52b514a7097582700d46170d87ce64e866fe625f
floatingman <dan@danlovesprogramming.com>
2024-10-10 14:50:41 -0500

    Updated plugins and changed codeium key bindings

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -1,12 +1,13 @@
 local globals = {
 	mapleader = " ",
-    maplocalleader = " ",
+	maplocalleader = " ",
+	codeium_disable_bindings = 1,
 	netrw_keepdir = 0,
 	netrw_banner = 0,
-    netrw_winsize = 30,
-    netrw_liststyle = 3,
+	netrw_winsize = 30,
+	netrw_liststyle = 3,
 	netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]],
-	netrw_localcopydircmd = 'cp -r',
+	netrw_localcopydircmd = "cp -r",
 }
 
 for k, v in pairs(globals) do
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -27,6 +27,7 @@ return {
 			-- sources for autocompletion
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" }, -- lsp
+                { name = "codeium" },
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
